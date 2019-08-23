081eae13b694934bb3e93f8dbd003fbb0d896047
Abi Ramya <abiramya.m@gmail.com>
2019-08-23 21:00:19 +0530

    Day One Task

---
+++ src/test/java/pushCode/Task_one.java
@@ -0,0 +1,19 @@
+package pushCode;
+
+public class Task_one {
+	
+
+		
+			public static void main  (String[]  args){
+			String Original = "GreensTechnologies";
+
+		StringBuffer SB = new StringBuffer(Original);
+		System.out.println(SB.reverse());
+		}
+		
+
+
+
+
+
+}
